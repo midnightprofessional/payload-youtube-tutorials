2f483ff7663765a9d66ec3fb172743195077f825
Mighnight Professional <ross.lin@gmail.com>
2025-02-10 13:19:27 +1100

    Lesson 2.5

--- src/app/(frontend)/posts/page.tsx
+++ src/app/(frontend)/posts/page.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { headers as getHeaders } from 'next/headers.js'
 import { getPayload } from 'payload'
 import config from '@/payload.config'
 
 export default async function PostsPage() {
   const payloadConfig = await config
-
   const payload = await getPayload({ config: payloadConfig })
+
+  const headers = await getHeaders()
+  const { user } = await payload.auth({ headers })
 
   const posts = await payload.find({
     collection: 'posts',
@@ -13,6 +16,11 @@ export default async function PostsPage() {
   })
 
   const [firstPost] = posts.docs
+
+  console.log({
+    user,
+    firstPost,
+  })
 
   return (
     <div>
