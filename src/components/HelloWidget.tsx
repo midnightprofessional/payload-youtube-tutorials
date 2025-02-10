ec00dc19443e41e863de9156f30a1a3867cd1fc2
Mighnight Professional <ross.lin@gmail.com>
2025-02-10 11:21:37 +1100

    Added Shadcn button

--- src/components/HelloWidget.tsx
+++ src/components/HelloWidget.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Button } from './ui/button'
 
 export default function HelloWidget() {
   return (
-    <div>
-      <div className="p-5 rounded-lg text-center text-5xl bg-[#29707a]">Hello Payload 🧨</div>
+    <div className="flex flex-row gap-3 items-center">
+      <div className=" flex-1 p-5 rounded-lg text-center text-5xl bg-[#29707a]">
+        Hello Payload 🧨
+      </div>
+      <Button variant="outline">Click me</Button>
     </div>
   )
 }
