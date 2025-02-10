bfb20cddccf9a0d49c8f971ab139983a40b34bed
Mighnight Professional <ross.lin@gmail.com>
2025-02-10 11:18:41 +1100

    Added Shadcn

--- src/components/HelloWidget.tsx
+++ src/components/HelloWidget.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
 export default function HelloWidget() {
-  return <div className="p-5 rounded-lg text-center text-5xl bg-[#29707a]">Hello Payload 🧨</div>
+  return (
+    <div>
+      <div className="p-5 rounded-lg text-center text-5xl bg-[#29707a]">Hello Payload 🧨</div>
+    </div>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ['class'],
   content: ['./src/**/*.{jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        lg: '0.5rem',
+        md: 'calc(0.5rem - 2px)',
+        sm: 'calc(0.5rem - 4px)',
+      },
+      colors: {},
+    },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 }
